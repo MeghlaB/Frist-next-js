5bf41c1ba3ebe30fe2217e624fcb3132f681c145
MeghlaB <meghlab964@gmail.com>
2025-03-11 15:42:42 +0600

    route

---
+++ src/app/Contact/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contactpage() {
+  return (
+    <div>
+      <p className="text-center text-green-700 font-semibold"> Contactpages</p>
+    </div>
+  );
+}
+
+export default Contactpage;
